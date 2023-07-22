d8097319190d22fe236d521cdba4b247deebb5c5
Huang Pan <huangpan@yahoo.com>
2023-07-22 15:14:10 -0700

    initial check in

---
+++ 4/EXERCISE-LL-Constructor.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+        self.tail = self.head
+        self.length = 1
+
+#mynode = Node(3)
+#print(mynode.value)
+#print(mynode.next)
+
+my_linked_list = LinkedList(4)
+
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length)
+print('Head:', id(my_linked_list.head))
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 4
+    Tail: 4
+    Length: 1
+    
+"""
+
+                                                                                                                    
