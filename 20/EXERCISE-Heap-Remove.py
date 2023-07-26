ede061b04d538f815b72a996a547202596bcfd81
Huang Pan <huangpan@yahoo.com>
2023-07-26 16:50:27 -0700

    update heap

--- 20/EXERCISE-Heap-Remove.py
+++ 20/EXERCISE-Heap-Remove.py
@@ -43,13 +43,18 @@ class MaxHeap:
             else:
                 return
                        
-    ## WRITE REMOVE METHOD HERE ##
-    #                            #
-    #                            #
-    #                            #
-    #                            #
-    ##############################
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
 
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+
+        return max_value
 
 
 myheap = MaxHeap()
