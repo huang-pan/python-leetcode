da4484445c6ac32e572bc3e42ef8ba46b8b90f6a
Huang Pan <huangpan@yahoo.com>
2023-07-29 19:41:30 -0700

    updated basic sorts

--- 26/EXERCISE-Bubble_Sort.py
+++ 26/EXERCISE-Bubble_Sort.py
@@ -1,14 +1,9 @@
-## WRITE BUBBLE_SORT FUNCTION HERE ##
-#                                   #
-#                                   #
-#                                   #
-#                                   #
-##################################### 
-    
-
-
-
-
+def bubble_sort(arr):
+    for i in range(len(arr)-1, 0, -1):
+        for j in range(0, i):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+    return arr
 
 print(bubble_sort([4,2,6,5,1,3]))
 
